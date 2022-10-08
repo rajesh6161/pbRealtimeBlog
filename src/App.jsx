f0ceb1d6bcaf9b81a6a3534cc78b68a531e5d0bd
rajesh6161 <rajeshranjan2712000@gmail.com>
2022-10-08 22:39:24 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TodoScreen from './components/TodoScreen';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <TodoScreen />
+    </div>
+  );
+}
+
+export default App;
