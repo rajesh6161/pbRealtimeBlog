b19e008c835b116a32a24ea792cb391b43c70e9d
rajesh6161 <rajeshranjan2712000@gmail.com>
2022-10-09 11:58:21 +0530

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,68 @@
-import TodoScreen from './components/TodoScreen';
+import { useEffect, useState } from 'react';
+import { DebounceInput } from 'react-debounce-input';
+import PocketBase from 'pocketbase';
 
 function App() {
+  const client = new PocketBase('http://127.0.0.1:8090');
+
+  const [realtimeWord, setRealtimeWord] = useState('');
+  const [recordId, setRecordId] = useState('');
+
+  const updateWord = async (rec_id, letter) => {
+    try {
+      await client.records.update('wordle', rec_id, {
+        word: letter,
+      });
+    } catch (error) {
+      console.log('Error occurred while updating the todo', error);
+    }
+  };
+
+  const initWord = async () => {
+    try {
+      let word = await client.records.getFullList('wordle', 1, {
+        sort: '-created',
+      });
+      if (word.length === 0) {
+        await client.records.create('wordle', {
+          word: '',
+        });
+        let w = await client.records.getFullList('wordle', 1, {
+          sort: '-created',
+        });
+        setRecordId(w[0].id);
+      } else {
+        setRecordId(word[0].id);
+      }
+    } catch (error) {
+      console.log('Error occurred while getting word', error.data);
+    }
+  };
+  console.log(recordId);
+  useEffect(() => {
+    client.realtime.subscribe('wordle', function (e) {
+      console.log('realtime', e.record);
+      setRealtimeWord(e.record);
+    });
+    return () => {
+      client.realtime.unsubscribe();
+    };
+  });
+
+  useEffect(() => {
+    initWord();
+  }, []);
+
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <TodoScreen />
+    <div className="flex flex-col justify-center items-center h-screen ">
+      <DebounceInput
+        minLength={1}
+        debounceTimeout={0}
+        onChange={(e) => {
+          updateWord(recordId, e.target.value);
+        }}
+      />
+      <p>{realtimeWord.word}</p>
     </div>
   );
 }
